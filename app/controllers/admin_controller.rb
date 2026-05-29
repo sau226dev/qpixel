1d2ada689d0fb7c6cb1420c76df7e331913baa8d
sau226 <32805612+sau226@users.noreply.github.com>
2026-05-29 15:18:04 +1000

    Bypass impersonation password check in dev

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -247,7 +247,7 @@ class AdminController < ApplicationController
       AuditLog.admin_audit(event_type: 'impersonation_end', related: current_user, user: @impersonator)
       sign_out @impersonator
       redirect_to new_saml_user_session_path
-    elsif @impersonator&.valid_password? params[:password]
+    elsif @impersonator&.valid_password? params[:password] || Rails.env.development?
       session.delete :impersonator_id
       AuditLog.admin_audit(event_type: 'impersonation_end', related: current_user, user: @impersonator)
       sign_in @impersonator
